406bd1a43c03861cf6a6474edd3705a779adcf8f
Kosstow <anastas.kosstow@protonmail.com>
2022-01-28 17:19:33 +0200

    Add route and cluster for Aggregator

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var yarpConfiguration = builder.Configuration.GetSection("yarp");
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(yarpConfiguration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Gateway");
+app.MapReverseProxy();
 app.Run();
