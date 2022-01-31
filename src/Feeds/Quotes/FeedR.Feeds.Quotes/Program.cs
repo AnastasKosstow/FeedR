d13de3f35e59e0277c40eadddf11355e445d4938
Kosstow <anastas.kosstow@protonmail.com>
2022-01-31 14:32:47 +0200

    fix PricingRequestChannel name

--- src/Feeds/Quotes/FeedR.Feeds.Quotes/Program.cs
+++ src/Feeds/Quotes/FeedR.Feeds.Quotes/Program.cs
@@ -1,6 +1,5 @@
 using FeedR.Feeds.Quotes.Extensions;
 using FeedR.Feeds.Quotes.Pricing.Factory;
-using FeedR.Feeds.Quotes.Pricing.Requests;
 using FeedR.Feeds.Quotes.Pricing.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,13 +13,13 @@ var app = builder.Build();
 app.MapGet("/", () => "FeedR Quotes");
 
 #region PricingBackgroundService
-app.MapPost("/pricing/start", async (PricingRequestsChannel channel, IPricingRequestFactory pricingFactory) =>
+app.MapPost("/pricing/start", async (PricingRequestChannel channel, IPricingRequestFactory pricingFactory) =>
 {
     await channel.Requests.Writer.WriteAsync(pricingFactory.StartRequest());
     return Results.Ok;
 });
 
-app.MapPost("/pricing/stop", async (PricingRequestsChannel channel, IPricingRequestFactory pricingFactory) =>
+app.MapPost("/pricing/stop", async (PricingRequestChannel channel, IPricingRequestFactory pricingFactory) =>
 {
     await channel.Requests.Writer.WriteAsync(pricingFactory.StopRequest());
     return Results.Ok;
